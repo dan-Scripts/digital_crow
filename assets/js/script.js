02c847d517896e880c376bfb490f1e1dc19af210
Daniel <danshmw@outlook.com>
2024-08-07 07:18:58 +0300

    Add testimonial section
    - add owl css stylsheets, and owl js files
    - add carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,30 @@ $(document).ready(function () {
     $(this).toggleClass("open");
     $(".mobileNav").toggleClass("active");
   });
+
+  // owlCarousel
+  $(".testimonials_content").owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 30,
+    dots: true,
+    autoplay: true,
+    smartSpeed: 300,
+    slideBy: 1,
+    responsive: {
+      // breakpoint from 0 up
+      0: {
+        items: 1,
+        autoplay: false,
+      },
+      // breakpoint from 480 up
+      768: {
+        items: 2,
+      },
+      // breakpoint from 768 up
+      991: {
+        items: 3,
+      },
+    },
+  });
 });
